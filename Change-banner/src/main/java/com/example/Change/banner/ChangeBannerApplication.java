a6e749c8f3acb86d3257c2957e05b3e578b2cc49
tayduasieucuong <0.199qui@gmail.com>
2023-01-04 18:55:23 +0700

    Change banner config link image

---
+++ Change-banner/src/main/java/com/example/Change/banner/ChangeBannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Change.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeBannerApplication.class, args);
+	}
+
+}
---
+++ Change-banner/src/test/java/com/example/Change/banner/ChangeBannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Change.banner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeBannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
